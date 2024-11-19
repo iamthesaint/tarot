07c72191e3512699a15f790bbed90778d8793c05
Phy <pham.phy@gmail.com>
2024-11-18 18:37:45 -0600

    savingReading in mutations

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import { gql } from '@apollo/client';
-
-
-export const SAVE_CARD = gql`
-    mutation saveCard($cardId: ID!) {
-        saveCard(cardId: $cardId) {
-        _id
-        username
-        email
-        savedCards {
-            _id
-            name
-            description
-            suit
-            uprightMeaning
-            reversedMeaning
-            image
-        }
-        }
-    }
-`;
-
-
-export const SAVE_READING = gql`
-    mutation saveReading($cards: [TarotCard] , $reflections: [String]) {
-        saveReading(cards: $cards, reflections: $reflections) {
-            _id
-            reflections
-            user {
-                _id
-                username
-            }
-        }
-    }
-`;
-
-
-=======
 import { gql } from "@apollo/client";
 
 // Mutation for logging in a user
@@ -66,41 +27,34 @@ export const ADD_USER = gql`
   }
 `;
 
-// // Mutation for saving a book to the user's savedBooks
-// export const SAVE_BOOK = gql`
-//   mutation saveBook($book: BookInput!) {
-//     saveBook(book: $book) {
-//       _id
-//       username
-//       email
-//       savedBooks {
-//         bookId
-//         title
-//         authors
-//         description
-//         image
-//         link
-//       }
+// export const SAVE_CARD = gql`
+//     mutation saveCard($cardId: ID!) {
+//         saveCard(cardId: $cardId) {
+//         _id
+//         username
+//         email
+//         savedCards {
+//             _id
+//             name
+//             description
+//             suit
+//             uprightMeaning
+//             reversedMeaning
+//             image
+//         }
+//         }
 //     }
-//   }
 // `;
 
-// // Mutation for removing a book from the user's savedBooks
-// export const REMOVE_BOOK = gql`
-//   mutation removeBook($bookId: ID!) {
-//     removeBook(bookId: $bookId) {
-//       _id
-//       username
-//       email
-//       savedBooks {
-//         bookId
-//         title
-//         authors
-//         description
-//         image
-//         link
-//       }
-//     }
-//   }
-// `;
->>>>>>> 708da2e2bdcd3a5b830a6a4a421e58a5b7e5e927
+export const SAVE_READING = gql`
+    mutation saveReading($cards: [TarotCard] , $reflections: [String]) {
+        saveReading(cards: $cards, reflections: $reflections) {
+            _id
+            reflections
+            user {
+                _id
+                username
+            }
+        }
+    }
+`;
