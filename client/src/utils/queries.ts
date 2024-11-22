56205e66b6097770e64d3fd193ad86d70e0eba7e
iamthesaint <stephenie2@me.com>
2024-11-22 15:46:18 -0500

    cleaned up some files

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -10,20 +10,3 @@ export const GET_ME = gql`
     }
   }
 `;
-<<<<<<< HEAD
-
-export const GET_TAROT_CARDS = gql`
-    query tarotCards {
-        tarotCards {
-        _id
-        name
-        description
-        suit
-        uprightMeaning
-        reversedMeaning
-        image
-        }
-    }
-`;
-=======
->>>>>>> 3c0392a9e290c35df51cd6ef293b3776859dd00e
