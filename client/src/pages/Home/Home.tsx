c750e62659c5be956b4f31f401e8aab4732dd108
Sabrina-Sawyer <sabrinagtc242@gmail.com>
2024-11-24 18:05:18 -0500

    added styling to home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,26 +1,25 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
-import divineLogoName from '../../assets/divineLogoName.png';
-import tarotHero from '../../assets/tarotHero.png';
+import divineLogo from '../../assets/divineLogo.png';
 
 const Home = () => {
   return (
     <div className="home">
       <textarea
         className="what-is-tarot"
-        placeholder="What is Tarot?"
+        placeholder="Tarot is a mystical deck of 78 cards, each brimming with stories and symbols. The Major Arcana reveal life’s big mysteries, while the Minor Arcana explore everyday adventures through four magical suits. A tarot reading is like a cosmic map, offering insight, guidance, and a touch of magic to those who seek it. "
       ></textarea>
       <img
-        src={divineLogoName}
+        src={divineLogo}
         alt="logo"
         className="logo"
       />
       <h1 className="home-title">Divine Deck</h1>
       <div className="home-container">
-        <p className="home-description">
-          Dive into the mystical world of tarot. Reflect, learn, and gain
-          insights through the wisdom of the cards.
-        </p>
+        {/* <p className="home-description">
+        Dive into the mystical world of tarot. Reflect, learn, and gain
+        insights through the wisdom of the cards.
+        </p> */}
         <Link to="/reading" className="btn-primary">
           Ask the Stars
         </Link>
