a365437ade7bc02cb60ad6981755f0395a284801
Sabrina-Sawyer <sabrinagtc242@gmail.com>
2024-11-24 17:34:21 -0500

    added images to home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,20 +1,31 @@
 import { Link } from "react-router-dom";
 import "./Home.css";
+import divineLogoName from '../../assets/divineLogoName.png';
+import tarotHero from '../../assets/tarotHero.png';
 
 const Home = () => {
   return (
     <div className="home">
+      <textarea
+        className="what-is-tarot"
+        placeholder="What is Tarot?"
+      ></textarea>
+      <img
+        src={divineLogoName}
+        alt="logo"
+        className="logo"
+      />
       <h1 className="home-title">Divine Deck</h1>
       <div className="home-container">
-          <p className="home-description">
-            Dive into the mystical world of tarot. Reflect, learn, and gain
-            insights through the wisdom of the cards.
-          </p>
-          <Link to="/reading" className="btn-primary">
-            Draw Tarot Cards
-          </Link>
-        </div>
+        <p className="home-description">
+          Dive into the mystical world of tarot. Reflect, learn, and gain
+          insights through the wisdom of the cards.
+        </p>
+        <Link to="/reading" className="btn-primary">
+          Ask the Stars
+        </Link>
       </div>
+    </div>
   );
 };
 
