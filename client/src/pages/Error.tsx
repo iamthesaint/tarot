357ecd66cd2945c5d88522b7a88ce10bfa716b2e
Sabrina-Sawyer <sabrinagtc242@gmail.com>
2024-11-25 14:45:05 -0500

    added styling to error page

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -4,9 +4,21 @@ import "./error.css";
 const Error: React.FC = () => {
   return (
     <div className="error-wrapper">
-      <h1>Error</h1>
-      <p>Page not found.</p>
-      <Link to="/">Go to homepage</Link>
+      <h1>404 | Oops! This page split like a banana.</h1>
+      {/* <p>Oops! This page split like a banana.</p> */}
+      <Link to="/"
+      className="home-btn">
+        Home
+      </Link>
+      <iframe
+        src="https://giphy.com/embed/H8uuXYln5pxVVLFn7k"
+        height="600"
+        width="900"
+        frameBorder="0"
+        allowFullScreen
+        title="Bananas GIF"
+        className="error-gif"
+      ></iframe>
     </div>
   );
 };
