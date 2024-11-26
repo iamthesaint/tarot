f504e81b598d01728d7370c8c9eb8b02de01221d
iamthesaint <stephenie2@me.com>
2024-11-26 00:22:47 -0500

    changed name for personal

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -13,12 +13,8 @@ const Home = () => {
         alt="logo"
         className="logo"
       />
-      <h1 className="home-title">Divine Deck</h1>
+      <h1 className="home-title">Ancient Arcana</h1>
       <div className="home-container">
-        {/* <p className="home-description">
-        Dive into the mystical world of tarot. Reflect, learn, and gain
-        insights through the wisdom of the cards.
-        </p> */}
         <Link to="/reading" className="btn-primary">
           Ask the Stars
         </Link>
